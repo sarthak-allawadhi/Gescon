649971bae3ddc321fd5edcd3ed026a246a6083ee
sarthakallawadhi1 <sarthakallawadhi@gmail.com>
2026-07-15 21:45:50 +0530

    style: remove Built with Reflex badge

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="mmuhg",
     api_url="http://localhost:8000",
+    show_reflex_badge=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
